3a42fae4bc77c5dfadc1032f6bd82676bd98f47f
huangtf <weixiaobaoyihao@163.com>
2020-09-28 14:33:18 +0800

    本地工程提交到git

---
+++ git-test-project/src/com/htf/git/FirstJava.java
@@ -0,0 +1,14 @@
+package com.htf.git;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
